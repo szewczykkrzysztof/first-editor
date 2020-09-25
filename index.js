20aa591f319d61d8ea47364729e35c86ae0b1bdd
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-09-25 14:42:25 +0200

    created function to save input content to local storage

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 
-document.cookie = 'name = Krzysiek';
-document.cookie = 'secondKey = drugie ciastko';
-console.log(document.cookie);
-
-localStorage.setItem('firstItem','Some Thing');
-
-console.log(localStorage.getItem('firstItem')); //pobiera zawartość klucza
-
-localStorage.removeItem('firstItem');   //kasuje klucz firstItem
-
-localStorage.setItem('Author','Krzysztof'); //zapisuje pod kluczem Author zawartość Krzysztof
-
-const owner = {
-    firstName : 'Krzysiek',
-    surename : 'Szewczyk',
-};
-
-console.log(owner);
-
-localStorage.setItem('Author',JSON.stringify(owner));  // Kodowanie obiektu do wewnętrznego formatu JSON
-
-console.log(localStorage.getItem('Author')); //Pobiera zakodowaną zawartość klucza 'Author'
-
-const pageAuthor = localStorage.getItem('Author');
-
-console.log(JSON.parse(pageAuthor));    //Odkodowuje zawartośc zmienej zakodowanej w formacie JSON
 
 const contentEditor = localStorage.getItem('content');  //przypina do stałej contentEditor zawartość klucza 'content', jeśli go nie ma zwraca wartość null
 
-let myEditor = '';    //tworzę zmienną myEditor z pustą zawartością
+let textBoxEditor = '';    //tworzę zmienną myEditor z pustą zawartością
 
 if (contentEditor) {
-    myEditor = contentEditor;
+    textBoxEditor = contentEditor;
 }
-// Jeśli contentEditor nie jest pusty, przypisz do zmiennej myEditor jego zawartość
+// Jeśli contentEditor nie jest pusty, przypisz do zmiennej textBoxEditor jego zawartość
+
+const text = document.querySelector('.editor--js'); //przypisuje do stałej text zawartość okienka edytora (klasa editor--js)
+
+text.value = textBoxEditor;
+
+console.log(text);
+
+const saveButton = document.querySelector('.save--js');
+
+console.log(saveButton);
+
+const openButton = document.querySelector('.open--js');
+
+console.log(openButton);
+
+
+saveButton.addEventListener("click", () => {
+    localStorage.setItem('content',text.value);
+  });
