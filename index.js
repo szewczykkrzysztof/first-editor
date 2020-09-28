1841d4762eb0d37b76af655d305b3c9b1882af14
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-09-28 13:18:21 +0200

    renamed constant name, removed unneded code (console logs, etc), add some comment

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
+const savedContent = localStorage.getItem("content");
+//przypina do stałej contentEditor zawartość klucza 'content', jeśli go nie ma zwraca wartość null
 
+let editorContent = ""; //tworzę zmienną z pustą zawartością
 
-const contentEditor = localStorage.getItem('content');  //przypina do stałej contentEditor zawartość klucza 'content', jeśli go nie ma zwraca wartość null
-
-let textBoxEditor = '';    //tworzę zmienną myEditor z pustą zawartością
-
-if (contentEditor) {
-    textBoxEditor = contentEditor;
+if (savedContent) {
+  editorContent = savedContent;
 }
-// Jeśli contentEditor nie jest pusty, przypisz do zmiennej textBoxEditor jego zawartość
+// Jeśli savedContent nie jest pusty, przypisz do zmiennej textBoxEditor jego zawartość
 
-const text = document.querySelector('.editor--js'); //przypisuje do stałej text zawartość okienka edytora (klasa editor--js)
+const editorWindow = document.querySelector(".editor--js"); //przypisuje do stałej editorWindow zawartość klasy editor--js
+const saveButton = document.querySelector(".save--js"); // tworze stałą i podpinam do niej klasę reprezentującą przycisk
+const loadButton = document.querySelector(".load--js"); // tworzę kolejny przycisk z przypisaną klasą
 
-text.value = textBoxEditor;
-
-console.log(text);
-
-const saveButton = document.querySelector('.save--js');
-
-console.log(saveButton);
-
-const loadButton = document.querySelector('.load--js');
-
-console.log(loadButton);
-
+editorWindow.value = editorContent; // zwracam  zawartość zmiennej editorContet jako zawartoś okna edytora
 
 saveButton.addEventListener("click", () => {
-    localStorage.setItem('content',text.value);
-  });
+  localStorage.setItem("content", editorWindow.value);
+});
 
+//podpinam do obiektu /przysisku zapisz/ akcję na klik, która zapisuje zawartość okna edytora do local storage pod kluczem content
